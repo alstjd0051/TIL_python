7513b0a8f188a4e89221f6784b0d814228b59964
TylerSong <wsc03002@naver.com>
2025-12-31 02:02:49 +0900

    Numpy (#2)
    * Add IDE configuration files for Python project, including .gitignore, module settings, and inspection profiles.
    
    * resetup
    
    * Update project dependencies and Python version; add requirements.txt and modify Jupyter notebooks for consistency. Adjust execution counts and fix paths in notebooks.

---
+++ src/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+
+# numpyFile = open("numpy.md", "w")
+# numpyFile.write(np.__doc__)
+# numpyFile.close()
+
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+
+print(os.getcwd())
